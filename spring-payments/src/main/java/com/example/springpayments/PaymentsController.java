3a15451829ac6f42bb79a08c5de509657beba1b8
HannyDuong <62577418+HannyDuong@users.noreply.github.com>
2021-11-13 14:59:44 -0800

    fixed import issue

--- spring-payments/src/main/java/com/example/springpayments/PaymentsController.java
+++ spring-payments/src/main/java/com/example/springpayments/PaymentsController.java
@@ -1,41 +1,22 @@
 package com.example.springpayments;
 
-import javax.validation.Valid;
-
 import com.example.springcybersource.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.net.InetAddress;
-import java.util.Optional;
-import java.time.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64.Encoder;
-import java.util.regex.Pattern;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.beans.factory.annotation.Value;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 
 @Slf4j
@@ -162,10 +143,10 @@ public class PaymentsController {
     public String getAction( @ModelAttribute("command") PaymentsCommand command, 
                             Model model) {
 
-        //return "creditcards" ;
+        return "creditcards" ;
         //return "login";
-        //return "register";
-        return "home";
+       // return "register";
+       // return "home";
 
     }
 
