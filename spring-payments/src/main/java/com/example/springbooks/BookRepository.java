c27316b4325795dd985b2e217c02d0007a551ea1
HannyDuong <ha.duong@sjsu.edu>
2021-11-18 00:08:14 -0800

    Re-structure the code to microservices

--- spring-payments/src/main/java/com/example/springbooks/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbooks;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface BookRepository extends JpaRepository<Book, Long> {
-    
-}
--- spring-payments/src/main/java/com/example/springbooks/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.springbooks;
-
-public class UserRepository {
-    
-}
