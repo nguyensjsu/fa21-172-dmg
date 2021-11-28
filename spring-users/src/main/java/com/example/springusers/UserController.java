0e216e914e7bdb6575c53e50ca53c8aa2e652b23
rheadash <rhea.dash@gmail.com>
2021-11-28 14:05:39 -0800

    Adding Kong files.

--- spring-users/src/main/java/com/example/springusers/UserController.java
+++ spring-users/src/main/java/com/example/springusers/UserController.java
@@ -38,7 +38,22 @@ public class UserController {
     @Autowired
     private UserRepository repository;
 
-    
+    class Ping {
+        private String test;
+
+        public Ping(String message) {
+            this.test = message;
+        }
+
+        public String getTest() {
+            return this.test;
+        }
+    }
+
+    @GetMapping("/ping")
+    public Ping ping() {
+        return new Ping("Spring Users API alive!");
+    }
 
     @GetMapping("/users")
     public ResponseEntity<User> getUser(@RequestParam(value = "email") String email, @RequestParam(value = "password") String password) throws ServerException {
