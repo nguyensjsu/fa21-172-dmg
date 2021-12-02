671d57b3fff935836abf359703dd6325f1a2c253
merrillpe <merrillpe@gmail.com>
2021-12-01 18:48:47 -0800

    Fix to initializations of databases

--- init/01.sql
+++ init/01.sql
@@ -1,8 +1,8 @@
-CREATE USER 'cmpe172'@'%' IDENTIFIED BY 'Abc@123$';
-GRANT ALL ON `cmpe172`.* to 'cmpe172'@'%';
+CREATE USER 'springuser'@'%' IDENTIFIED BY 'Abc@123$';
+GRANT ALL ON `cmpe172`.* to 'springuser'@'%';
 CREATE DATABASE IF NOT EXISTS `springpayments`;
-GRANT ALL ON `springpayments`.* TO 'cmpe172'@'%';
+GRANT ALL ON `springpayments`.* TO 'springuser'@'%';
 CREATE DATABASE IF NOT EXISTS `springusers`;
-GRANT ALL ON `springusers`.* TO 'cmpe172'@'%';
+GRANT ALL ON `springusers`.* TO 'springuser'@'%';
 CREATE DATABASE IF NOT EXISTS `springbooks`;
-GRANT ALL ON `springbooks`.* TO 'cmpe172'@'%';
+GRANT ALL ON `springbooks`.* TO 'springuser'@'%';
