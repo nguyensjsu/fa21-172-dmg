38a994d3da9acf651b1087f60def2d90c8c31e15
rheadash <rhea.dash@gmail.com>
2021-12-04 14:31:06 -0800

    Removed old yaml files, unneeded code from back office.

--- backoffice/src/main/java/com/example/backoffice/BackofficeController.java
+++ backoffice/src/main/java/com/example/backoffice/BackofficeController.java
@@ -108,13 +108,7 @@ public class BackofficeController {
         return "login_suc";
     }
 
-    // @GetMapping("/register_null")
-    // public String registerNull(@ModelAttribute("user") User user,
-    // Model model)  {
-        
-    //     System.out.println("Accessing register_null");
-    //     return "register_null";
-    // }
+
     
     @GetMapping("/reset_dne")
     public String resetDNE(@ModelAttribute("user") User user,
@@ -139,14 +133,6 @@ public class BackofficeController {
         System.out.println("Accessing reset_suc");
         return "reset_suc";
     }
-
-    // @GetMapping("/register_suc")
-    // public String registerSucceeded(@ModelAttribute("user") User user,
-    // Model model)  {
-        
-    //     System.out.println("Accessing register_suc");
-    //     return "register_suc";
-    // }
 
     @PostMapping
     public String login(@Valid @ModelAttribute("user") User user,  
@@ -177,38 +163,7 @@ public class BackofficeController {
             return "home";
         }
     }
-
-    // @GetMapping("/register")
-    // public String getRegister( @ModelAttribute("user") User user,
-    //                        Model model) {
-    //     System.out.println("Accessing registration");
-    //     return "register";
-    // }
-    
-    // @PostMapping("/register")
-    // public String postAction(@Valid @ModelAttribute("user") User user,  @RequestParam(value="action", required=false) String action, Errors errors, Model model, HttpServletRequest request) {
-    //     log.info(" User : " + user) ;
-    //     ResponseEntity<User> response = restTemplate.postForEntity(SPRING_USERS_URI + "/users", user, User.class);
-    //     // if (existingUser != null) {
-    //     //     throw new ResponseStatusException(HttpStatus.FORBIDDEN, "That email is already registered!");
-    //     // }
-    //     System.out.println("frontend/register");
-    //     System.out.println("response " + response.toString());
-    //     System.out.println("Headers " + response.getHeaders());
-    //     if (response.getHeaders().getFirst("status").equals(HttpStatus.INTERNAL_SERVER_ERROR + "")) {
-    //         return "register_null";
-    //     }
-
-    //     else if (response.getHeaders().getFirst("status").equals(HttpStatus.CONFLICT + "")) {
-    //         return "register_alr";
-    //     }
-    //     else if (response.getHeaders().getFirst("status").equals(HttpStatus.OK + "")){
-    //         return "register_suc";
-    //     }
-    //     else {
-    //         return "register";
-    //     }          
-    // }
+  
 
     @GetMapping("/passwordreset")
     public String getResetPassword( @ModelAttribute("user") User user,
@@ -251,91 +206,6 @@ public class BackofficeController {
     }
 
 
-    /*
-    Spring Books
-    */
-
-    // @GetMapping("/catalog")
-    // public String getCatalog (@ModelAttribute("command") BookCommand command,
-    //                         Model model) {
-    //     System.out.println("Accessing catalog");
-    //     return "catalog";
-    // }
-
-    // @PostMapping("/catalog")
-    // public String addToCart(@ModelAttribute("command") BookCommand command, 
-    //                         @RequestParam(value="action", required=true) String action, 
-    //                         Model model, HttpServletRequest request) {
-    //     log.info( "Action: " + action);
-
-    //     ResponseEntity<BookCommand> response = restTemplate.postForEntity(SPRING_BOOKS_URI + "/catalog?bookID=" + action + "&qty=" + command.getQuantity(action), command, BookCommand.class);
-
-    //     return "catalog";
-    // }
-
-    // @GetMapping("/shoppingcart") 
-    // public String getCart (Model model) {
-    //     System.out.println("Accessing shopping cart");
-
-    //     ArrayList<CartItem> items = new ArrayList<CartItem>();
-
-    //     ResponseEntity<ArrayList> response = restTemplate.getForEntity(SPRING_BOOKS_URI + "/shoppingcart", ArrayList.class, items);
-    //     log.info("Frontend Response: " + response.toString());
-        
-    //     ObjectMapper mapper = new ObjectMapper();
-
-    //     for (Object item : response.getBody())
-    //         try{
-    //             //JsonNode jsonNode = mapper.readTree(response.getBody().toString());
-    //             //System.out.print("JSON: " + jsonNode);
-    //             CartItem newItem = new CartItem();
-
-    //             newItem = mapper.convertValue(item, CartItem.class);
-    //             items.add(newItem);
-    //         } catch ( Exception e ) { 
-    //             System.out.println( e ) ; 
-    //         }
-
-    //     float subtotal = 0;
-    
-    //     for (CartItem item : items) {
-    //         subtotal += item.getBook().getPrice() * item.getQuantity();
-    //     }
-
-    //     model.addAttribute("items", items);
-    //     model.addAttribute("subtotal", String.valueOf(subtotal));
-        
-
-    //     log.info("Frontend books: " + items.toString());
-        
-    //     return "shoppingcart";
-    // }
-
-    // @PostMapping("/shoppingcart")
-    // public void postCart(@RequestParam(value="action", required=true) String action, 
-    //                     Model model) {
-        
-    //     log.info("Action: " + action);
-
-    //     ResponseEntity<String> response = restTemplate.postForEntity(SPRING_BOOKS_URI + "/shoppingcart?action=" + action, action, String.class);
-
-    //     getCart(model);
-        
-    //     /*
-    //     if(action.equals("checkout")) {
-            
-    //     } else if(action.equals("clear")) {
-    //         ResponseEntity<String> response = restTemplate.postForEntity(SPRING_BOOKS_URI + "/shoppingcart", action, String.class);
-    //     } else {
-            
-    //     }
-    //     */
-    // }
-
-     /* 
-    Spring Payments
-    */
-
     double total = 25;
     int min = 1239871 ;
     int max = 9999999 ;
@@ -352,150 +222,5 @@ public class BackofficeController {
     String exp;
     String phone;
     String email;
-
-
-//     @GetMapping("/creditcards")
-//     public String getAction(@ModelAttribute("command") PaymentsCommand command,
-//                             Model model) {
-//         log.info("Command: " + command);
-// //        PaymentsCommand paym = restTemp.getForObject(SPRING_PAYMENTS_URI + "/creditcards", command, PaymentsCommand.class);
-
-//         model.addAttribute("order_number", order_num);
-//         model.addAttribute("total", total);
-//         model.addAttribute("fname", fname);
-//         model.addAttribute("lname", lname);
-//         model.addAttribute("address", address);
-//         model.addAttribute("city", city);
-//         model.addAttribute("state", state);
-//         model.addAttribute("zip", zip);
-//         model.addAttribute("phone", phone);
-
-//         model.addAttribute("card_num", cardnum);
-//         model.addAttribute("card_balance",balance);
-//         model.addAttribute("exp", exp);
-//         model.addAttribute("email", email);
-//         return "creditcards";
-//     }
-//     @PostMapping("/creditcards")
-//     public PaymentsCommand postAction(@Valid @ModelAttribute("command") PaymentsCommand command,
-//                                       @RequestParam(value="action", required=true) String action,
-//                                       Errors errors, Model model, HttpServletRequest request) {
-
-//         log.info("Action: " + action);
-//         log.info("Command: " + command);
-//         PaymentsCommand payment = restTemplate.postForObject(SPRING_PAYMENTS_URI + "/command", command, PaymentsCommand.class);
-//         command.setTransactionAmount( 300.00) ;
-//         fname = command.getFirstname();
-//         lname = command.getLastname();
-//         address = command.getAddress();
-//         city = command.getCity();
-//         state = command.getState();
-//         zip = command.getZip();
-//         balance = command.getTransactionAmount();
-//         cardnum = command.getCardnumber();
-//         exp = command.getExpmonth();
-//         exp = exp + "/";
-//         exp = exp + command.getExpyear();
-//         phone = command.getPhone();
-//         email = command.getEmail();
-
-//         model.addAttribute("order_number", order_num);
-//         model.addAttribute("total", total);
-//         model.addAttribute("fname", command.getFirstname() );
-//         model.addAttribute("lname", command.getLastname());
-//         model.addAttribute("address", command.getAddress() );
-//         model.addAttribute("city", command.getCity() );
-//         model.addAttribute("state", command.getState() );
-//         model.addAttribute("zip", command.getZip());
-//         model.addAttribute("phone", command.getPhone());
-
-//         model.addAttribute("card_num", command.getCardnumber());
-//         model.addAttribute("card_balance", command.getTransactionAmount());
-//         model.addAttribute("exp", exp);
-
-
-//         return payment;
-//     }
-
-    @Getter
-    @Setter
-    class Message {
-        private String msg;
-
-        public Message(String m) {
-            msg = m;
-        }
-    }
-
-    class ErrorMessages {
-        private ArrayList<Message> messages = new ArrayList<Message>();
-
-        public void add(String msg) {
-            messages.add(new Message(msg));
-        }
-
-        public ArrayList<Message> getMessages() {
-            return messages;
-        }
-
-        public void print() {
-            for (Message m : messages) {
-                System.out.println(m.msg);
-            }
-        }
-    }
-
-
-
-//     @GetMapping("/placeorder")
-//     public String getPlaceOrder( PaymentsCommand command, Model model ){
-//         log.info("Accessing place order method " );
-
-// //        balance = command.getTransactionAmount();
-//         model.addAttribute("firstname", fname);
-//         model.addAttribute("lastname", lname);
-//         model.addAttribute("address", address);
-//         model.addAttribute("city", city);
-//         model.addAttribute("state", state);
-//         model.addAttribute("zip", zip);
-//         model.addAttribute("phone", phone);
-
-//         model.addAttribute("card_num", cardnum);
-//         model.addAttribute("card_balance",balance);
-//         model.addAttribute("exp", exp);
-//         model.addAttribute("email", email);
-//         return "placeorder";
-
-//     }
-
-//     @PostMapping("/placeorder")
-//     public PaymentsCommand postOrder(@Valid @ModelAttribute("command") PaymentsCommand command,
-//                             @RequestParam(value="placeorder", required=true) String action,
-//                             Errors errors, Model model, HttpServletRequest request) {
-// //    public String postOrder(@Valid @ModelAttribute("command") PaymentsCommand command,
-// //                                     @RequestParam(value="action", required=false) String action,
-// //                                     Errors errors, Model model, HttpServletRequest request) {
-
-//         log.info("Accessing place order method " );
-// //        ResponseEntity<PaymentsCommand> res = restTemp.postForEntity(SPRING_PAYMENTS_URI + "/placeorder?email=" + command.getEmail(), command, PaymentsCommand.class);
-
-//         PaymentsCommand pay = restTemplate.postForObject(SPRING_PAYMENTS_URI + "/placeorder?email=" + command.getEmail(), command, PaymentsCommand.class);
-//         balance = balance - total;
-//         model.addAttribute("firstname", fname);
-//         model.addAttribute("lastname", lname);
-//         model.addAttribute("address", address);
-//         model.addAttribute("city", city);
-//         model.addAttribute("state", state);
-//         model.addAttribute("zip", zip);
-//         model.addAttribute("phone", phone);
-
-//         model.addAttribute("card_num", cardnum);
-//         model.addAttribute("card_balance",balance);
-//         model.addAttribute("exp", exp);
-// //        model.addAttribute("email", email);
-//         log.info("Balance:"  + balance);
-
-//         return pay;
-//     }
 
 }
